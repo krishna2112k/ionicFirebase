bf51224d462ce6e7c72dfa8399e33d0e5550b77d
vamsi krishna <vamsi@azrisolutions.com>
2019-07-02 15:57:27 +0530

    intitial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private firebase: Firebase,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.initializeFirebase();
+    });
+  }
+
+  initializeFirebase() {
+    this.firebase.getToken()
+    .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
+    .catch(error => console.error('Error getting token', error));
+
+    this.firebase.onNotificationOpen()
+      .subscribe(data => console.log(`User opened a notification ${data}`));
+
+    this.firebase.onTokenRefresh()
+      .subscribe((token: string) => console.log(`Got a new token ${token}`));
+  }
+}
